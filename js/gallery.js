1463f36f345aa60fbafa252dd91f31336cb05e2a
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-11-20 23:20:23 +0200

    добавляет открытие и закрытие модального окна

--- js/gallery.js
+++ js/gallery.js
@@ -4,6 +4,10 @@ import galleryItems from './gallery-items.js';
 // Создание и рендер разметки по массиву данных и предоставленному шаблону
 const galleryList = document.querySelector('.js-gallery');
 
+const lightbox = document.querySelector('.js-lightbox');
+
+const lightboxImage = document.querySelector('.lightbox__image');
+
 const galleryListItem = galleryItem => {
     const li = document.createElement('li');
     li.classList.add('gallery__item');
@@ -26,4 +30,42 @@ const galleryListItem = galleryItem => {
 const galleryImages = galleryItems.map(galleryItem => galleryListItem(galleryItem));
 
 galleryList.append(...galleryImages);
-console.log(galleryList);
+// console.log(galleryList);
+
+galleryList.addEventListener('click', openLightbox);
+
+function openLightbox(event) {
+    event.preventDefault();
+    window.addEventListener('keydown', onPressEscape);
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    lightbox.classList.add('is-open');
+    lightboxImage.src = event.target.dataset.source;
+}
+
+// закрытие модального окна по клику на кнопку
+const closeLightbox = document.querySelector('button[data-action="close-lightbox"]');
+
+closeLightbox.addEventListener('click', onCloseModal);
+
+function onCloseModal() {
+    window.removeEventListener('keydown', onPressEscape);
+    lightbox.classList.remove('is-open');
+}
+
+// Закрытие модального окна по клику на div.lightbox__overlay   
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+
+lightboxOverlay.addEventListener('click', event => {
+    if (event.target === event.currentTarget) {
+        onCloseModal();
+    }
+});
+
+// Закрытие модального окна по нажатию клавиши ESC
+function onPressEscape(event) {
+    if (event.code === 'Escape') {
+            onCloseModal();
+        }
+}
